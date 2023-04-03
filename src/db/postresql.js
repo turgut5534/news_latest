b374508619531ea01e1f50ff6dd5a448b02d0bc5
turgut5534 <turgutsalgin5534@gmail.com>
2023-04-03 03:26:43 +0300

    asddsa

--- src/db/postresql.js
+++ src/db/postresql.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize')
 
-const dbName = "turgut";
-const dbUser = "postgres";
-const dbPass = "Busra!keles!1";
-const dbHost = "localhost";
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
 
 const sequelize = new Sequelize(dbName, dbUser, dbPass, {
     host: dbHost,
